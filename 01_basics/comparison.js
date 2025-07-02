dc5135a487931f8d16be396ac8abeecdbbb70dd8
Kishan749 <138244961+Kishan749@users.noreply.github.com>
2025-07-02 18:01:19 +0000

    comparsion

---
+++ 01_basics/comparison.js
@@ -0,0 +1,19 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// ******* these are little tricky comparison of two different Datatypes
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null>0) //  (0>0) false
+// console.log(null==0)// (undefined==0) flase
+// console.log(null >= 0)// 0>=0 true
+
+console.log("2" == 2)// ==, which does type coercion if the operands are different types.
+console.log("2" === 2)// no type coercion
+
+
+
