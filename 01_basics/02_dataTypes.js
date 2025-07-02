13477a46925841d81e43c7e20aae0d04febea398
Kishan749 <138244961+Kishan749@users.noreply.github.com>
2025-07-02 15:24:44 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";// treat all JS code as newer version
+
+//alert(3+3)// we are using nodeJs, not browser so it will not work here
+
+console.log(3 + 3)
+
+// Datatypes
+
+let name = "Baki"               // string 
+let age = 19                    // number
+let isLogedIn = false           // boolean
+
+// number=>2 to power 53
+// bigint
+// string
+// boolean
+// null=> standalone value
+// undefined=>
+// symbol=> unique
+
+// object
+
+console.log(typeof"BAki")
+console.log(typeof 343)
+console.log(typeof null)
