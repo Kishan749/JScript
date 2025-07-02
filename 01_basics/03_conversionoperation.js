2ed5c8f9efdbd4997cc5c1907c635bcc9c9b51b2
Kishan749 <138244961+Kishan749@users.noreply.github.com>
2025-07-02 17:35:04 +0000

    4

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,57 @@
+let score = "44";
+let no = "494dkd"// type=> Number , 
+let score2 = null// type=> 0
+let score3 = undefined// NaN
+let score4= true // 1
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+let valueInNumber1 = Number(no)
+
+// console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber1);
+
+// console.log(valueInNumber1);// NaN
+
+// "33" => 33
+// "33df" =>NaN
+// true => 1 , false => 0
+// 1 => true , 0 => false
+
+// strings case
+
+let someNumber = 383
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+//******************* Operations ***************************
+
+let value = 3 
+let negvalue = -value
+// console.log(negvalue)
+
+let str1 = "Baki"
+let str2 = " Hanma"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(true)
+
+let gameCounter = 100
+gameCounter++
+
+console.log(gameCounter)//101
+
+
+
