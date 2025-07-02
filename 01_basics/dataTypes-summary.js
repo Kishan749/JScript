2aceff02c3840d2505c549a46a162d2fe3e647a4
Kishan749 <138244961+Kishan749@users.noreply.github.com>
2025-07-02 19:05:40 +0000

    datatype-summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,38 @@
+// Primitive
+
+// 7 Types : Strings, Boolean , null ,undefined, Symbol , BigInt, Number
+
+// Non Primitive(Reference type)
+
+// Array , objects, functions
+
+const hero = ["hulk", "loki", "doga"]
+// console.log(typeof (hero))
+
+let myObj = {
+    name: "Kishan",
+    age: 22
+}
+// console.log(typeof(myObj))
+
+
+// functions stored in variables
+
+const myFunc = function (){
+    console.log("hello world")
+}
+console.log(typeof (myFunc))
+    
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
